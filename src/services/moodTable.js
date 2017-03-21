d640756422b0cf3985abed95317b242fd95a9080
jal5313 <jal5313@rit.edu>
2017-03-21 09:43:44 -0400

    started implementing mood analysis. Non funcitonal, scafolding

---
+++ src/services/moodTable.js
@@ -0,0 +1,132 @@
+const moodTable = {
+    //Arousal
+    low: {
+        //Temper
+        low: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        medium: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        high: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        }
+    },
+    //Arousal
+    medium: {
+        //Temper
+        low: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        medium: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        high: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        }
+    },
+    //Arousal
+    high: {
+        //Temper
+        low: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        medium: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        },
+        //Temper
+        high: {
+            //Valence
+            low: {
+                moodID: 0
+            },
+            medium: {
+                moodID: 0
+            }, 
+            high: {
+                moodID: 0
+            }
+        }
+    }
+}
+
+module.exports = {
+    moodTable: moodTable,
+}
--- src/services/resultParser.js
+++ src/services/resultParser.js
@@ -1,10 +1,32 @@
+const moodTableService = require('./moodTable.js');
+
 const analyzeBeyondVerbal = (analysis) => {
     console.log(analysis);
     console.log('===Doing Magic===');
     
     var responseJSON = {
-        moodID: 1,
+        moodID: 0,
     }
+    
+    var arousal = analysis.analysisSegments[0].analysis.Arousal;
+    var temper = analysis.analysisSegments[0].analysis.Temper;
+    var valence = analysis.analysisSegments[0].analysis.Valence;
+    
+    responseJSON.moodID = moodTableService.moodTable[arousal][temper][valence].moodID;
+    
+    /*
+    test = {
+        Group: "medium",
+        Summary: {
+            Mean: "34.77",
+            Mode: "medium",
+            ModePct: 100
+        },
+        Value: "34.77"
+    }
+    
+    */
+    
     
     return responseJSON;
 }
