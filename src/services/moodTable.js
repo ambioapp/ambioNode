3446af003f10451c39f52b7ad76da84defc88c09
jal5313 <jal5313@rit.edu>
2017-03-28 08:53:38 -0400

    best guess and BV terms of mood groups

--- src/services/moodTable.js
+++ src/services/moodTable.js
@@ -1,17 +1,17 @@
 const analyzeATV = (arousal, temper, valence) => {
     if (arousal.Group === 'low') {
         if (temper.Group === 'low') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 111,
                     value: 'depressed',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 112,
                     value: 'calm',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 113,
                     value: 'content',
@@ -23,17 +23,17 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'medium') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 121,
                     value: 'sad',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 122,
                     value: 'neutral',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 123,
                     value: 'happy'
@@ -45,19 +45,19 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'high') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 131,
                     value: 'angry',
                 };
             }
-            else if (valence.Group === 'medium') {
+            else if (valence.Group === 'neutral') {
                 return {
                     code: 132,
                     value: 'excited',
                 };
             }
-            else if (valence.Group === 'high') {
+            else if (valence.Group === 'positive') {
                 return {
                     code: 133,
                     value: 'joyful',
@@ -74,19 +74,19 @@ const analyzeATV = (arousal, temper, valence) => {
                 value: 'err: 100',
             };
         }
-    } else if (arousal.Group === 'medium') {
+    } else if (arousal.Group === 'mid') {
         if (temper.Group === 'low') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 211,
                     value: 'depressed',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 212,
                     value: 'calm',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 213,
                     value: 'content',
@@ -98,17 +98,17 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'medium') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 221,
                     value: 'sad',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 222,
                     value: 'neutral',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 223,
                     value: 'happy',
@@ -120,19 +120,19 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'high') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 231,
                     value: 'angry',
                 };
             }
-            else if (valence.Group === 'medium') {
+            else if (valence.Group === 'neutral') {
                 return {
                     code: 232,
                     value: 'excited',
                 };
             }
-            else if (valence.Group === 'high') {
+            else if (valence.Group === 'positive') {
                 return {
                     code: 233,
                     value: 'joyful',
@@ -151,17 +151,17 @@ const analyzeATV = (arousal, temper, valence) => {
         }
     } else if (arousal.Group === 'high') {
         if (temper.Group === 'low') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 311,
                     value: 'depressed',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 312,
                     value: 'calm',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 313,
                     value: 'content',
@@ -173,17 +173,17 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'medium') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 321,
                     value: 'sad',
                 };
-            } else if (valence.Group === 'medium') {
+            } else if (valence.Group === 'neutral') {
                 return {
                     code: 322,
                     value: 'neutral',
                 };
-            } else if (valence.Group === 'high') {
+            } else if (valence.Group === 'positive') {
                 return {
                     code: 323,
                     value: 'happy',
@@ -195,19 +195,19 @@ const analyzeATV = (arousal, temper, valence) => {
                 };
             }
         } else if (temper.Group === 'high') {
-            if (valence.Group === 'low') {
+            if (valence.Group === 'negative') {
                 return {
                     code: 331,
                     value: 'angry',
                 };
             }
-            else if (valence.Group === 'medium') {
+            else if (valence.Group === 'neutral') {
                 return {
                     code: 332,
                     value: 'excited',
                 };
             }
-            else if (valence.Group === 'high') {
+            else if (valence.Group === 'positive') {
                 return {
                     code: 333,
                     value: 'joyful',
