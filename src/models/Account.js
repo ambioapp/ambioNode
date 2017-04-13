71c711c0752a672930f19de5dc74b340f6b8851e
jal5313 <jal5313@rit.edu>
2017-04-13 10:03:43 -0400

    adding models folder and account schema

---
+++ src/models/Account.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let AccountModel = {};
+
+const AccountSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+});
+
+AccountModel = mongoose.model('Account', AccountSchema);
+
+module.exports.AccountModel = AccountModel;
+module.exports.AccountSchema = AccountSchema;
